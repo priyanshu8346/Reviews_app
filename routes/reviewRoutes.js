b859e2a3fc169fe9a93f40d5671a8be7fec1af79
Priyanshu Agrawal <priyanshuagrawal@Priyanshus-MacBook-Air.local>
2025-09-05 22:43:52 +0530

    create review solved

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -7,7 +7,7 @@ const admin = require('../middleware/admin');
 const { createReview, getReviews, getMyLatestReview, updateReview, deleteReview} = require('../controllers/reviewController');
 
 router.post('/', auth, createReview);
-router.get('/', getReviews);
+router.get('/getReviews', admin, getReviews);
 router.get('/my-latest', auth, getMyLatestReview);
 router.put('/:reviewId', auth, updateReview);    // Edit review
 router.delete('/:reviewId', auth, deleteReview); // Delete review
